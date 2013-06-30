64cd81458b47ebe3802bbd45e3d283f1568cb34e
Terry Hibbert <terry.hibbert@apparent.com.au>
2013-07-01 08:53:07 +1000

    Breakpoints tweak; Some JS refactoring; Added paulirish' matchMedia for some IE9 love;

--- js/land/land.js
+++ js/land/land.js
@@ -1,8 +1,21 @@
-var LaND = function() {
-    var that = this;
+/*
+ * A script to compliment the LaND CSS framework.
+ * Watches for LaND media query changes and applies layout abbreviation to HTML element.
+ * NB - It's working in recent Chrome. Add paulirish's matchMedia for some IE 9 support.
+ */
 
-    this.layouts = {}
-    ;
+var LaND = function(options) {
+    var that = this;
+    var options = typeof options !== 'undefined' ? options : {};
+
+    var defaults = {
+        'applyToHTML': true
+    };
+
+    options = $.extend(defaults, options);
+
+    this.layouts = {};
+
     var listeners = {
         change: {}
     };
@@ -10,11 +23,82 @@ var LaND = function() {
     this.init = function() {
         console.log("LaND init...");
 
+        if (options.applyToHTML) addHTMLaddListeners();
+
+        findMediaQueries();
+
+        return this;
+    }
+
+    this.addListener = function(name, handler) {
+        if (!listeners.hasOwnProperty(name)) {
+            throw "Land doesn't have an event called '" + name + "'";
+            return false;
+        }
+
+        listeners[name][handler] = handler;
+
+        return that;
+    }
+
+    this.layoutChanges = function() {
+        var result = {
+            'add': [],
+            'remove': []
+        };
+        var layouts = this.layouts;
+
+        for (la in layouts) {
+            if (layouts.hasOwnProperty(la)) {
+                if (layouts[la]) {
+                    result.add.push('La-'+la);
+                } else {
+                    result.remove.push('La-'+la);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    function addHTMLaddListeners() {
+        var $html = $('html');
+        that.addListener(LaND.events.CHANGE, function() {
+            var changes = that.layoutChanges();
+            //console.log('Change event caught:');
+            //console.log(changes);
+            $html.removeClass(changes.remove.join(' '));
+            $html.addClass(changes.add.join(' '));
+        });
+    }
+
+    function dispatch(name, args) {
+        var list = listeners[name];
+
+        for (var prop in list) {
+            if(list.hasOwnProperty(prop)){
+                list[prop].apply(this, args);
+            }
+        }
+    }
+
+    function handleMediaChange(active, layouts) {
+        //console.log(layouts);
+        //console.log(active);
+
+        var layoutLength = layouts.length;
+        for (var i=0; i<layoutLength; ++i) {
+            that.layouts[layouts[i]] = active;
+        }
+
+        dispatch('change', []);
+    };
+
+    function findMediaQueries() {
         var sheetsLength = document.styleSheets.length;
         var sheet;
 
         for (var i=0; i<sheetsLength; ++i) {
-
             sheet = document.styleSheets[i];
 
             if (sheet.href.indexOf('/land.') > -1) {
@@ -23,18 +107,6 @@ var LaND = function() {
                 var rules = sheet.cssRules,
                     numRules = rules.length,
                     mqls = {};
-
-                var mediaChange = function (active, layouts) {
-                    //console.log(layouts);
-                    //console.log(active);
-
-                    var layoutLength = layouts.length;
-                    for (var i=0; i<layoutLength; ++i) {
-                        that.layouts[layouts[i]] = active;
-                    }
-
-                    dispatch('change', []);
-                };
 
                 for (var j = 0; j < numRules; j += 1) {
                     if (rules[j].constructor === CSSMediaRule) {
@@ -50,36 +122,14 @@ var LaND = function() {
                         mqls['mql' + j] = window.matchMedia(rules[j].media.mediaText);
                         mqls['mql' + j].addListener(function(mql, layouts) {
                             return function() {
-                                mediaChange(mql.matches, layouts);
+                                handleMediaChange(mql.matches, layouts);
                             }
                         }(mqls['mql' + j], layouts));
 
-                        mediaChange(mqls['mql' + j].matches, layouts);
+                        handleMediaChange(mqls['mql' + j].matches, layouts);
                     }
                 }
                 break;
-            }
-        }
-        return this;
-    }
-
-    this.addListener = function(name, handler) {
-        if (!listeners.hasOwnProperty(name)) {
-            throw "Land doesn't have an event called '" + name + "'";
-            return false;
-        }
-
-        listeners[name][handler] = handler;
-
-        return that;
-    }
-
-    function dispatch(name, args) {
-        var list = listeners[name];
-
-        for (var prop in list) {
-            if(list.hasOwnProperty(prop)){
-                list[prop].apply(this, args);
             }
         }
     }
