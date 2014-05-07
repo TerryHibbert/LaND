1db97be4794f84b0888b3712e2d68646ffecebda
Terry Hibbert <terry.hibbert@apparent.com.au>
2014-05-08 08:57:08 +1000

    Added protocol check to bootstrap (for ie shims);

--- land.php
+++ land.php
@@ -6,6 +6,10 @@
     $LaND_enable_js = false; // Not ready yet
     $LaND_media = 'all';
 
+    $LaND_protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443)
+        ? "https"
+        : "http";
+
     $LaND_debug = false;
 
     function land_debug($message, $renderScriptTags = false) {
@@ -16,24 +20,24 @@
     }
 ?>
 <!-- BEGIN LaND -->
-<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/normalize.css">
+<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/normalize.css"/>
 
 <!--[if lt IE 9]>
-<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<script src="<?php echo $LaND_protocol; ?>://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 <!--[if lt IE 8]>
 <script src="<?php echo $url_path_to_land; ?>js/json2.js"></script>
 <![endif]-->
 <!--[if lt IE 7]>
-<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
+<script src="<?php echo $LaND_protocol; ?>://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
 <![endif]-->
 
-<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-mobile.css">
+<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-mobile.css"/>
 
-<!--[if lte IE 8]><link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-desktop.css"><![endif]-->
-<!--[if IE 9]><link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-respond.css"><![endif]-->
+<!--[if lte IE 8]><link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-desktop.css"/><![endif]-->
+<!--[if IE 9]><link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-respond.css"/><![endif]-->
 <!--[if !IE | gte IE 10]><!-->
-<style>@media all and (min-width:1px) {.mediatest{position:absolute;}}</style>
+<style type="text/css">@media all and (min-width:1px) {.mediatest{position:absolute;}}</style>
 <script>
     <?php land_debug('LaND start'); ?>
 
@@ -53,7 +57,7 @@
 
         if (mm.matches) {
             <?php land_debug('LaND matches min width'); ?>
-            document.write('<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-respond.css">');
+            document.write('<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-respond.css"/>');
             <?php land_debug('LaND loaded responsive CSS'); ?>
         } else {
             <?php land_debug('LaND doesn\'t match min width'); ?>
@@ -69,16 +73,16 @@
 
         if (typeof CSSMediaRule !== 'undefined') {
             <?php land_debug('LaND no matchMedia, but CSS media queries are supported'); ?>
-            document.write('<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-respond.css">');
+            document.write('<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-respond.css"/>');
             <?php land_debug('LaND loaded responsive CSS'); ?>
         } else {
             <?php land_debug('LaND no matchMedia and no CSS media queries supported'); ?>
-            document.write('<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-desktop.css">');
+            document.write('<link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-desktop.css"/>');
             <?php land_debug('LaND loaded desktop CSS'); ?>
         }
     }
 </script>
-<noscript><link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-desktop.css"></noscript>
+<noscript><link rel="stylesheet" type="text/css" media="<?php echo $LaND_media; ?>" href="<?php echo $url_path_to_land; ?>css/land-desktop.css"/></noscript>
 <!--<![endif]-->
 
 <?php if ($LaND_enable_js) : ?>
