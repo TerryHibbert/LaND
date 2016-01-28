b8d200d6a4b1ea05494e635ee040f1d7d72f0b53
Terry Hibbert <terry.hibbert@apparent.com.au>
2016-01-28 17:43:03 +1100

    Start V2 work;
    Create an experimental SASS include to move the classes out of the HTML markup;
    Add an SASS include test page;

---
+++ v2/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>LaND - Include</title>
+
+    <link rel="stylesheet" href="/assets/css/site-included.css">
+</head>
+<body>
+    <div class="layout layout_default">
+        <header>
+            <h1>LaND - Include</h1>
+            <nav>
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                </ul>
+            </nav>
+        </header>
+
+        <article>
+            <main>
+                main
+            </main>
+
+            <aside>
+                aside
+            </aside>
+        </article>
+
+        <footer>
+            footer
+        </footer>
+    </div>
+</body>
+</html>
